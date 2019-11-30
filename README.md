9ed04c3bfae754ab5f4e882443d53eb33d0f2f24
Francisco Cerqueira <57841600+xico2001pt@users.noreply.github.com>
2019-11-30 13:14:26 +0000

    Added Week 02 code
    Added spaceship and its icon and navigation system.

--- README.md
+++ README.md
@@ -19,35 +19,48 @@ O jogador perde quando é atingido pelos adversários três vezes.
 #### Pacotes
 
 - Pygame
+- Random
 
 #### Tarefas
 
 1. ~**MATRIZ PARA O MAPA**~
-   1. fazer uma matriz de inteiros (tamanho do mapa=32x8), em que teremos:
+   1. fazer uma matriz binária (tamanho do mapa=32x8), em que teremos:
       * vazio=0
       * parede=1
    1. Desenhar os objetos no ecrã
       * para já cada objeto deverá ter 25x25 porque assim vai ocupar 25*32=800 pixeis, que é um tamanho razoável
       * colocar pontos de rescue ao longo do mapa
-1. **DESENHAR ECRÃ CIMA**
+1. ~**DESENHAR ECRÃ CIMA**~
    1. Desenhar nave
-       * Sugiro tentar manter o tamanho das coisas múltiplos de 25
-       * Ou seja, a nave poderia ser 75x50
    1. Desenhar também no mapa
-1. **TECLAS**
+1. ~**TECLAS**~
    * Navegação da nave usando as teclas de cursor
 1. **COLISÃO NO MAPA**
    1. Não deixar nave mover se for contra alguma parede no mapa
    1. Quando chega a um rescue, desenhar 5 inimigos no ecrã de cima
+      * Recolher posição do jogador para decidir em qual lado os inimigos aparecem
+      * Usar aleatoriedade para decidir a direção que se desloca
    1. Transportar o jogador quando este chega aos extremos
 1. **COLISÃO ECRÃ DE CIMA**
-   1. Quando existe colisão nave com inimigo => perde uma vida
+   1. Quando existe colisão da nave com inimigo, o jogador perde uma vida
    1. Nave pode disparar
       * existe uma lista com posição dos tiros
       * existe outra lista com direcção dos tiros
       * usar esta informação para desenhar e mover os tiros em cada ciclo
-   1. Quando existe colisão tiro com inimigo => inimigo desaparece
+   1. Quando existe colisão tiro com inimigo, o inimigo desaparece
 1. **DESENHAR ESTRELAS / DESENHAR VIDAS**
-1. **MENU + CONSOLA**
+   * Implementar sistema de vida do jogador
+   * Adicionar tempo de recuperação após perder uma vida
+   * O nível termina quando não existem mais vidas
+1. **MENU**
+1. **CONSOLA**
+   1. Aspetos Gráficos
+   1. Configurações
+      * FPS: Configurar FPS do jogo
+      * Style: Alterar o estilo e as cores do jogo
+   1. Pontuação
+      * Adicionar contador com a pontuação do jogador
+1. **EFEITOS SONOROS + RESTART**
+1. **BÓNUS: Tornado**
 
 18/11/2019
--- flashgordon-atari.py
+++ flashgordon-atari.py
@@ -3,8 +3,13 @@ import pygame
 WIDTH = 800
 HEIGHT = 600
 FPS = 30
-WALL = pygame.image.load('./assets/textures/wall.png')
+
+# load
+# ICON = pygame.image.load('./assets/textures/icon.png')
 RESCUE = pygame.image.load('./assets/textures/rescue.png')
+SPACESHIP = pygame.image.load('./assets/textures/spaceship.png')
+rescue_points = [(380, 405), (330, 505), (260, 480), (80, 460), (85, 535), (115, 410),
+                 (710, 435), (660, 460), (530, 430), (535, 510), (560, 485)]
 MAP_MATRIX = [[1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
               [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
               [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
@@ -12,44 +17,77 @@ MAP_MATRIX = [[1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
               [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
               [1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1],
               [1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-              [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-              ]
+              [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1]]
+# physics
+XS0, YS0 = 350, 200
+XIS0, YIS0 = 395, 485
+xs, ys = XS0, YS0
+xis, yis = XIS0, YIS0
+SPACESHIP_VEL = 0.15
+SPACESHIP_ICON_VEL = 0.02
+right_side = True
 
 # definir cores
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-GREY = (180,180,180)
+GREY = (180, 180, 180)
+BLUE = (0, 85, 212)
+BROWN = (170, 100, 30)
 
 # inicialização do jogo
 pygame.init()
 pygame.mixer.init()  # permite som
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("New Flash Gordon")
+# pygame.display.set_icon(ICON)
 clock = pygame.time.Clock()
 
 # game loop
 running = True
 while running:
-    clock.tick(FPS)
+    dt = clock.tick(FPS)
+    
     # ler eventos
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    if keys[pygame.K_UP] or keys[pygame.K_w]:
+        ys -= SPACESHIP_VEL * dt
+        yis -= SPACESHIP_ICON_VEL * dt
+    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+        ys += SPACESHIP_VEL * dt
+        yis += SPACESHIP_ICON_VEL * dt
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        xs -= SPACESHIP_VEL * dt * 1.5
+        xis -= SPACESHIP_ICON_VEL * dt * 1.5
+        right_side = False
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        xs += SPACESHIP_VEL * dt * 1.5
+        xis += SPACESHIP_ICON_VEL * dt * 1.5
+        right_side = True
+    if keys[pygame.K_SPACE]:
+        xs, ys = XS0, YS0
+        xis, yis = XIS0, YIS0
+
     # atualizar
+    spaceship_pos = (xs, ys)
     
     # render
     screen.fill(BLACK)
     for row in range(8):
         for col in range(30):
-            if MAP_MATRIX[row][col] == 0:
-                pass
-            elif MAP_MATRIX[row][col] == 1:
-                screen.blit(WALL, (25+25*col, 375+25*row))
-    for x, y in [(380, 405), (330, 505), (260, 480), (80, 460), (85, 535),(115, 410),
-                 (710, 435), (660, 460), (530, 430), (535, 510), (560, 485)]:
+            if MAP_MATRIX[row][col]:
+                pygame.draw.rect(screen, BROWN, (25+25*col, 375+25*row, 25, 25), 0)
+    for x, y in rescue_points:
         screen.blit(RESCUE, (x, y))
+    if right_side:
+        screen.blit(SPACESHIP, spaceship_pos)
+    else:
+        screen.blit(pygame.transform.flip(SPACESHIP, True, False), spaceship_pos)
+    pygame.draw.rect(screen, BLUE, (xis,yis,10,5), 0)
+    
+    # consola
     pygame.draw.rect(screen, WHITE, (0,393-25,800,7), 0)
     pygame.draw.rect(screen, GREY, (0,0,25,800), 0)
     pygame.draw.rect(screen, GREY, (0,600-25,800,25), 0)
